d530f7bea7bfb06165a58d1c71bd9a583e37b96b
DoriCzele <dorottya.czele@gmail.com>
2022-06-21 22:20:38 +0100

    Setup basket context and render to page

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,23 +1,23 @@
 from products.models import Product
 
 
-def basket_contents(request):
-    basket_items = request.session.get("basket", [])
+def basket_context(request):
     basket_price = 0
+    basket_items = [{"id": 1, "quantity": 2}, {"id": 2, "quantity": 2}]
 
-    for item, item_quantity in basket_items:
+    for item in basket_items:
         try:
-            product = Product.objects.get(id=item, active=True)
+            product = Product.objects.get(id=item["id"], active=True)
             if product is not None:
-                basket_price += product.price * item_quantity
+                basket_price += product.price * item["quantity"]
             else:
                 basket_items.pop(item)
         except Product.DoesNotExist:
             basket_items.pop(item)
 
-    request.session["basket"] = basket_items
-
-    return {
+    request.session["basket"] = {
         "basket_items": basket_items,
-        "basket_price": basket_price,
+        "basket_price": str(basket_price),
     }
+
+    return request.session["basket"]
--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+
+def basket(request):
+    basket = request.session.get(
+        "basket", {"basket_contents": [], "basket_price": 0})
+    basket_items = basket["basket_items"]
+    item_ids = [item["id"] for item in basket_items]
+
+    products = list(Product.objects.filter(
+        id__in=item_ids).values("id", "name", "price"))
+
+    for product in products:
+        # Cast decimal to string for render
+        product["price"] = str(product["price"])
+        for item in basket_items:
+            if product["id"] == item["id"]:
+                product["quantity"] = item["quantity"]
+
+    return render(request, 'basket.html', {"products": products})
