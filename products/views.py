b0914464b2fa007bbf3c0e6d0b7135084fae6827
DoriCzele <dorottya.czele@gmail.com>
2022-06-21 18:44:47 +0100

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -8,6 +8,14 @@ class ProductListView(list.ListView):
     model = Product
     paginate_by = 6
 
+    def get_queryset(self):
+        search_string = self.request.GET.get("search", None)
+        if search_string:
+            product_list_queryset = Product.objects.filter(
+                name__icontains=search_string)
+            return product_list_queryset
+        return super().get_queryset()
+
 
 class ProductDetailView(detail.DetailView):
 
