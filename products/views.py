357188b2f4e87f699e91a2bf1ee33d5270c5840a
DoriCzele <dorottya.czele@gmail.com>
2022-06-25 16:34:27 +0100

    Fix search bar functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, reverse
 from django.views.generic import list, detail
 from .models import Product
 
@@ -7,6 +7,14 @@ class ProductListView(list.ListView):
 
     model = Product
     paginate_by = 6
+
+    def get(self, request, *args, **kwargs):
+        search_string = self.request.GET.get("search", None)
+        if search_string == "":
+            # Remove search param when empty search submitted
+            return redirect(reverse("product-list"))
+
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
         search_string = self.request.GET.get("search", None)
