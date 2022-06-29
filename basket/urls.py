c9d5bd28fc4d4472fdbd2852d819c71509e9533e
DoriCzele <dorottya.czele@gmail.com>
2022-06-29 20:17:39 +0200

    Update basket urls

--- basket/urls.py
+++ basket/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import basket, add_basket_item, modify_existing_items
+from basket.views import basket, add_basket_item, modify_existing_items
 
 urlpatterns = [
     path("", basket, name="basket"),
