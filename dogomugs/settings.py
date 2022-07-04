a318856bc1165f849406f600b885732522467ff9
DoriCzele <dorottya.czele@gmail.com>
2022-07-04 20:08:16 +0100

    Make debug an environmental variable

--- dogomugs/settings.py
+++ dogomugs/settings.py
@@ -27,7 +27,7 @@ development = os.environ.get("DEVELOPMENT", False)
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [".herokuapp.com",
                  "dogomugs.herokuapp.com", "localhost", "127.0.0.1"]
