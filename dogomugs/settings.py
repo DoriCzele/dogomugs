9c93b088b821936dd737373ba5ae7cb383c9852b
DoriCzele <dorottya.czele@gmail.com>
2022-07-08 19:01:16 +0100

    Make allowed hosts more strict

--- dogomugs/settings.py
+++ dogomugs/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = [".herokuapp.com",
-                 "dogomugs.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["dogomugs.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
